28b7a7d3f396fdad5365f7ab6cdc10aa7d5c1f3c
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2022-12-15 22:38:48 +0100

    finished complete logic

--- assets/scripts/myscript.js
+++ assets/scripts/myscript.js
@@ -1,31 +1,36 @@
 
-document.querySelector("#insert").addEventListener("keydown", event => {
-    if (event.key === "Enter") {
-        console.log("u press enter")
-        insertDiv()
+// document.querySelector("#insert").addEventListener("keydown", event => {
+//     if (event.key === "Enter") {
+//         console.log("u press enter")
+//         insertDiv()
+//     }
+// })
+// button conecction add
+document.querySelector("#button-add").addEventListener("click", event => {
+    // console.log("u clicked")
+    insertDiv()
+})
+// delete function
+document.querySelector("#button-delete").addEventListener("click", () => {
+    const listOfClass = document.getElementsByClassName("try-red")
+    for (let i = 0; i < listOfClass.length; i++) {
+        listOfClass[i].textContent = ""
     }
 })
-document.querySelector("#button").addEventListener("click", event => {
-    console.log("u clicked")
-    insertDiv()
-
-})
-
-
+// logic add div
 insertDiv = () => {
-    console.log("u run insertDiv")
+    // console.log("u run insertDiv")
     const nuovoDiv = document.createElement("div")
-    console.log(nuovoDiv)
+    // console.log(nuovoDiv)
     nuovoDiv.textContent = document.getElementById("insert").value
-    console.log(nuovoDiv)
+    // console.log(nuovoDiv)
 
 
 
     nuovoDiv.addEventListener("click", function () {
-        nuovoDiv.style.textDecoration = "line-throught"
+        nuovoDiv.classList.toggle("try-red")
     })
-
-
-
     document.querySelector("#container-lista").appendChild(nuovoDiv)
+    document.querySelector("#insert").value = ""
 }
+
